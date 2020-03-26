337626c5f434ecf555cc19f838c86b3b8b6670b4
Jeff Bliss <jeff.a.bliss@gmail.com>
2020-03-26 11:38:44 -0400

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,9 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <div>
+      <Link to="/map/">Go to southfact map</Link>
+    </div>
   </Layout>
 )
 
