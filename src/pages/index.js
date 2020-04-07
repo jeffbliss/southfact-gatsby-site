45ec03544c11d49b10b08f2fa3511ddd565d15f4
Jeff Bliss <jeff.a.bliss@gmail.com>
2020-04-07 08:58:24 -0400

    Completely redoing everything and starting as barebones as possible

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,21 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import '../components/index.css'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-    <div>
-      <Link to="/map/">Go to southfact map</Link>
-    </div>
-  </Layout>
+  <React.Fragment>
+    <h1>
+      Southern Forest Area Change Tools - SouthFACT
+    </h1>
+    <h3>
+      <ul className='nav'>
+        <li>Home</li>
+        <li>Map</li>
+        <li>Custom Requests</li>
+        <li>Learn</li>
+        <li>Contact</li>
+      </ul>
+    </h3>
+  </React.Fragment>
 )
 
 export default IndexPage
