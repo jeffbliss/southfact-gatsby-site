97b097708edc3535d204b1ed848c87cad8b013b5
Jeff Bliss <jeff.a.bliss@gmail.com>
2020-05-20 11:05:52 -0400

    added grid to appbar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Link from '../components/Link';
 
 const useStyles = makeStyles((theme) => ({
@@ -32,25 +31,30 @@ export default function ButtonAppBar() {
           Southern Forest Area Change Tools - SouthFACT
         </h2>
         <Toolbar mr={10}>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
-            <Link to='/' color='inherit'>
-              Home
-            </Link>
-            <Link to='/map' color='inherit'>
-              Map
-            </Link>
-            <Link to='/customRequests' color='inherit'>
-              Custom Requests
-            </Link>
-            <Link to='/learn' color='inherit'>
-              Learn
-            </Link>
-            <Link to='/contact' color='inherit'>
-              Contact
-            </Link>
+            <Grid
+              container
+              direction="row"
+              justify="space-between"
+              alignItems="flex-start"
+              lg="5"
+            >
+              <Link to='/' color='inherit'>
+                Home
+              </Link>
+              <Link to='/map' color='inherit'>
+                Map
+              </Link>
+              <Link to='/customRequests' color='inherit'>
+                Custom Requests
+              </Link>
+              <Link to='/learn' color='inherit'>
+                Learn
+              </Link>
+              <Link to='/contact' color='inherit'>
+                Contact
+              </Link>
+            </Grid>
           </Typography>
         </Toolbar>
       </AppBar>
--- src/pages/map.js
+++ src/pages/map.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Link from "../components/Link"
 import ButtonAppBar from '../components/AppBar'
 
 const map = () => (
