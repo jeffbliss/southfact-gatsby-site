23a7c42af5ab9fa76942533b1ac8cf8acb491c52
Jeff Bliss <jeff.a.bliss@gmail.com>
2020-05-29 11:54:35 -0400

    trying another way

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -15,7 +15,9 @@ const faq = ({
         <Typography variant="h5">
           {edge.node.frontmatter.title}
         </Typography>
-        <Typography variant="body1" dangerouslySetInnerHTML={{ __html: `<div>${edge.node.html}</div>` }}/>
+        <div>
+          <Typography variant="body1" dangerouslySetInnerHTML={{ __html: edge.node.html }}/>
+        </div>
       </React.Fragment>
     )
   return (
